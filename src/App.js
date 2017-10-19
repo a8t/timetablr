eac5ac33dbeb985e800cfdbad2df61064b4c3330
Andy Tran <andy8tran@gmail.com>
2017-10-18 22:54:14 -0400

    refactored add and remove section data methods

--- src/App.js
+++ src/App.js
@@ -13,16 +13,18 @@ class App extends Component {
   }
 
   addMeetingSectionData(newMeetingData) {
-    if (this.state.meetingSectionData.reduce((n, val) => n + (val.code === newMeetingData.code && val.courseCode === newMeetingData.courseCode), 0) < 2) {
-      this.setState({
-        meetingSectionData: [...this.state.meetingSectionData, newMeetingData]
-      })
+    if (this.state.meetingSectionData.reduce((n, val) => n + (val.code === newMeetingData.code && val.courseCode === newMeetingData.courseCode), 0) >= 2) {
+      return
     }
+
+    this.setState({
+      meetingSectionData: [...this.state.meetingSectionData, newMeetingData]
+    })
   }
   
   removeMeetingSectionData(meetingDataToRemove) {
     this.setState((prevState) => {
-      const index = prevState.meetingSectionData.indexOf(meetingDataToRemove)
+      const index = prevState.meetingSectionData.findIndex(i => i.courseCode === meetingDataToRemove.courseCode && i.code === meetingDataToRemove.code)
       prevState.meetingSectionData.splice(index,1)
 
       return {meetingSectionData: prevState.meetingSectionData}
