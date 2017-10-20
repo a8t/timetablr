503f6dacc117ac8dfa9ee095fd052c857d55ad75
sbcreates <drawsarahb@gmail.com>
2017-10-20 11:54:26 -0400

    Fixed calendar entry fonts for print media to fit in div on print

--- src/Components/Calendar/Entry/Entry.js
+++ src/Components/Calendar/Entry/Entry.js
@@ -3,16 +3,16 @@ import './Entry.css';
 
 const Entry = props => {
   return (
-    <div 
-      style={props.style} 
+    <div
+      style={props.style}
       className="entry"
-    > 
+    >
       <button
         className="removeEntry"
-        onClick={(e) => props.removeMeetingSectionData({ code: props.code, courseCode: props.courseCode })}  
+        onClick={(e) => props.removeMeetingSectionData({ code: props.code, courseCode: props.courseCode })}
       >x</button>
       <p className="entryText courseCode">{props.courseCode}</p>
-      <p className="entryText">{props.code}</p>
+      <p className="entryText meetingSection">{props.code}</p>
     </div>
   )
 }
