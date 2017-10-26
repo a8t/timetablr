26a241e88d6080452d478d5969ba91adf3b05fc9
sbcreates <drawsarahb@gmail.com>
2017-10-26 17:36:19 -0400

    Created modal login and styled remove buttons

---
+++ src/Components/Navbar/Login/Login.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import './Login.css';
+
+class Login extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      displayLogin: false
+    }
+  }
+
+  toggleLogin() {
+    this.setState({
+      displayLogin: !this.state.displayLogin
+    })
+  }
+
+  render() {
+  return (
+    <div className='loginContainer'>
+      <button
+        onClick={() => this.toggleLogin()}
+        className="login"
+        href="#modal">LOGIN / SIGNUP</button>
+
+      <div id="modal"
+        className="modalContainer"
+        style={{ display: this.state.displayLogin ? 'block' : 'none', overflow: 'auto'}}>
+
+        <div className="popupContainer">
+          <button class="closeForm"
+            onClick={() =>  this.toggleLogin()}>x</button>
+
+          <header className="popupHeader">Login</header>
+
+          <div className="popupForm">
+            <label className="label form">Email/Username</label>
+            <input type="text" className="block form input" required />
+            <label className="label form">Password</label>
+            <input type="password" className="block form input" required />
+            <input type="checkbox" className="form" />
+            <label className="label form rememberMe">Remember me on this computer</label>
+            <button className="block form submit" type="text">SUBMIT</button>
+            <button className="form register" type="text">REGISTER</button>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  )}
+}
+
+export default Login
